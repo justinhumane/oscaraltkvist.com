79d3265593700577ec31df359b332cc005f88f8c
Oscar Altkvist <oscar.altkvist@gmail.com>
2024-02-26 15:46:29 +0100

    Add target blank to icon links

--- components/IconLink.vue
+++ components/IconLink.vue
@@ -4,7 +4,7 @@ defineProps(["name", "href"]);
 
 <template>
   <div>
-    <NuxtLink class="flex items-center gap-2 hover:text-sky-500 tranistion duration-150" :to="href">
+    <NuxtLink class="flex items-center gap-2 hover:text-sky-500 tranistion duration-150" :to="href" target="_blank">
       <Icon :name="name" size="2rem" />{{ href }}</NuxtLink
     >
   </div>
