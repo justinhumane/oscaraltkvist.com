26983522ecc2a38107ce2217b728cd4947dc1688
Oscar Altkvist <oscar.altkvist@gmail.com>
2024-02-26 15:54:07 +0100

    Add text prop to icon link

--- components/IconLink.vue
+++ components/IconLink.vue
@@ -1,11 +1,11 @@
 <script setup>
-defineProps(["name", "href"]);
+defineProps(["name", "text", "href"]);
 </script>
 
 <template>
   <div>
     <NuxtLink class="flex items-center gap-2 hover:text-sky-500 tranistion duration-150" :to="href" target="_blank">
-      <Icon :name="name" size="2rem" />{{ href }}</NuxtLink
+      <Icon :name="name" size="2rem" />{{ text }}</NuxtLink
     >
   </div>
 </template>
