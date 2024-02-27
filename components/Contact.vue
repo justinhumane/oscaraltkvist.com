b1d352e54a08c64462c8193c54b49ff99558e855
Oscar Altkvist <oscar.altkvist@gmail.com>
2024-02-27 12:38:06 +0100

    Contact mobile design fix.

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,7 +2,7 @@
   <div class="px-8 py-16 contact">
     <h2 id="contact" class="scroll-mt-28"><span class="text-sky-500 font-bold">/</span>contact</h2>
     <article class="pt-8 pb-32">
-      <div class="border-sky-500 border-t-2 pt-4 grid grid-cols-4 gap-16">
+      <div class="border-sky-500 border-t-2 pt-4 gap-16">
         <slot />
       </div>
     </article>
